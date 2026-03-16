8032fa78c3249565261d6bb0dc83adfc6a77645a
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2026-03-16 13:27:51 -0600

    Add Home component with template and styles; update index title

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { Home } from "./home/home";
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [/* RouterOutlet, */ Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
